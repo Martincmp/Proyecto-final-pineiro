eea02cfdf35c258c69a77652fea88c6393a4881b
Carlos Piñeiro <martincmp77@gmail.com>
2022-10-29 22:36:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemListContainer from './components/ItemList/ItemListContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './components/Cart/Cart';
+import Checkout from './components/Checkout';
+import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
+import { CartProvider } from './contexts/CartContext';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
+    return (
+      <BrowserRouter basename='/reactjs38100'>
+        <CartProvider >
+          <NavBar />
+            <Routes className="fondo">
+              <Route path='/' element={<ItemListContainer greeting={'Tienda De Maquinas Para Gimnasios'} />} />
+              <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Tienda De Maquinas Para Gimnasios'} />} />
+              <Route path='/item/:id' element={<ItemDetailContainer />} />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/checkout' element={<Checkout />} />
+            </Routes>
+        </CartProvider>
+      </BrowserRouter>
+    );
 }
 
 export default App;
